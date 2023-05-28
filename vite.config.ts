96b68e1bf3d152dd5a30f9af4ef522ae09c04fbe
Guz013 <43732358+Guz013@users.noreply.github.com>
2023-05-28 20:51:18 -0300

    feat: 🧑‍💻 add auto imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,46 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import AutoImport from 'unplugin-auto-import/vite';
+import SvelteImport from 'unplugin-svelte-components/vite';
 import UnoCss from 'unocss/vite';
 
 export default defineConfig({
 	plugins: [
+		SvelteImport({
+			include: [
+				/\.svelte/,
+				/\.svelte\?svelte/,
+				/.[tj]sx?$/,
+			],
+			dirs: [
+				'./src/lib',
+			],
+			dts: './src/components.d.ts',
+		}),
+		AutoImport({
+			include: [
+				/\.svelte/,
+				/\.svelte\?svelte/,
+				/.[tj]sx?$/,
+			],
+			imports: [
+				'svelte',
+				'svelte/animate',
+				'svelte/easing',
+				'svelte/motion',
+				'svelte/store',
+				'svelte/transition',
+			],
+			dirs: [
+				'./src/lib',
+			],
+			dts: './src/imports.d.ts',
+			eslintrc: {
+				enabled: true,
+				filepath: './src/.eslintrc-auto-import.json',
+				globalsPropValue: true,
+			},
+		}),
 		UnoCss(),
 		sveltekit(),
 	],
