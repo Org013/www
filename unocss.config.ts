d16f61618d422dfdc1c646565063a7c3c2a18101
Guz013 <43732358+Guz013@users.noreply.github.com>
2023-05-28 20:42:25 -0300

    feat: ➕ add UnoCss

---
+++ unocss.config.ts
@@ -0,0 +1,42 @@
+import {
+	defineConfig,
+	presetAttributify,
+	presetIcons,
+	presetUno,
+	presetWebFonts,
+	transformerDirectives,
+	transformerVariantGroup,
+} from 'unocss';
+import extractorSvelte from '@unocss/extractor-svelte';
+
+export default defineConfig({
+	presets: [
+		presetUno(),
+		presetAttributify(),
+		presetIcons({
+			collections: {
+				solar: () => import('@iconify-json/solar/icons.json').then(i => i.default as any),
+				spinners: () => import('@iconify-json/svg-spinners/icons.json').then(i => i.default as any),
+			},
+		}),
+		presetWebFonts({
+			fonts: {
+				poppins: {
+					name: 'Poppins',
+					provider: 'bunny',
+				},
+				sans: [{
+					name: 'Cal Sans',
+					provider: 'none',
+				}],
+			},
+		}),
+	],
+	transformers: [
+		transformerDirectives(),
+		transformerVariantGroup(),
+	],
+	extractors: [
+		extractorSvelte(),
+	],
+});
