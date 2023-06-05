119f324f7d18fe0df141d7355074ad26321c027f
Guz013 <43732358+Guz013@users.noreply.github.com>
2023-06-05 11:24:48 -0300

    chore(deps): 🔗 add mdsvex for markdown support

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	extensions: ['.svelte', '.svx', '.md'],
+	preprocess: [vitePreprocess(), mdsvex({
+		extensions: ['.svx', '.md'],
+	})],
 	kit: {
 		adapter: adapter(),
 	},
