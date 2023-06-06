a591174d24050eff0b5fa61cb2949fa0638aa8cc
Guz013 <43732358+Guz013@users.noreply.github.com>
2023-06-05 22:18:55 -0300

    feat: ✨ basic landing page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,21 @@
 	import '@fontsource-variable/inter';
 	import 'cal-sans';
 	import 'virtual:uno.css';
+	import '../app.scss';
 </script>
+
+<main
+	un-bg="#020202"
+	un-text="gray12"
+	un-font="inter"
+	un-w="screen"
+	un-min-h="screen"
+	un-top="0"
+	un-left="0"
+	un-fixed
+	un-flex
+	un-justify="center"
+	un-items="center"
+>
+	<slot />
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<section un-max-w="60ch">
+	<h1 un-font="sans bold" un-text="7xl">Someone who's trying to improve</h1>
+
+	Hello world! I am ~~Gustavo L. de Mello~~ Guz,
+	a software developer and generalist currently working on contributing and creating open-source software and libraries.
+
+	Focusing and specializing in [Vite](https://vitejs.dev), [Svelte](https://svelte.dev)/[SvelteKit](https://kit.svelte.dev)
+	and their ecosystems, while creating web and mobile applications for experience, fun and hopefully to be useful for someone.
+
+	Using my own anxiety to loose sleep because of a mind that doesn't stop having ideas is how I work. And you can check out
+	how this workflow is going on my social medias or on platforms such as [GitHub](https://github.com/Guz013) where most of the projects are public to anyone
+	who wants to see the progress of them.
+
+	Outside ~~loosing sleep because~~ of programming, I also create designs and logos said projects. And when not working on them I also enjoy
+	creating art on any type of media, like drawings, pixelart, 3D, photography, etc. My photos can be found on [Instagram](https://instagram.com/guz013_)
+	or [Pixelfed](https://pixelfed.social/guz013), digital arts are on dedicated accounts.
+</section>
